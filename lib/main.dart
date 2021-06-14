be2df252e15eea14814cb76c646f146dfa518ebb
ozodtagoev <ozod.tagoev@gmail.com>
2021-06-14 16:31:38 +0500

    Stateful widgets for each screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'pages/home.dart';
+import 'pages/choose_location.dart';
+import 'pages/loading.dart';
 
 void main() => runApp(MaterialApp(
-
+  home: Home(),
 ));
